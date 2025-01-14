5627633be4077f25c53a288c4378a68b0531a37e
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2025-01-14 23:09:17 +0300

    TodoApp: Alembic.

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -6,15 +6,20 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-# SessionLocal = sessionmaker(autoflush=False, bind=engine)
+# SQLite
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345@localhost/TodoApplicationDatabase'
+# PostgreSQL
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:12345@localhost/TodoApplicationDatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# MySQL
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:12345@127.0.0.1:3306/TodoApplicationDatabase'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
@@ -28,3 +33,5 @@ def get_db():
 
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+# sqlite3 .\todosapp.db
--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,4 +1,5 @@
 import logging
+
 from fastapi import FastAPI
 
 from .models import Base
