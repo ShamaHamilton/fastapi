fd423a49dc92f53d147e1ed3159bfc8ff557bb62
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2025-01-25 22:50:59 +0300

    Blog site: fullstack application completed.

---
+++ fastapi-blog/frontend/src/App.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+
+import Post from './components/Post'
+import NewPost from './components/NewPost'
+
+const BASE_URL = 'http://localhost:8000/'
+
+function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch(BASE_URL + 'post/all')
+      .then((response) => {
+        const json = response.json()
+        console.log(json)
+        if (response.ok) {
+          return json
+        }
+        throw response
+      })
+      .then((data) => {
+        return data.reverse()
+      })
+      .then((data) => {
+        setPosts(data)
+      })
+      .catch((error) => {
+        console.log(error)
+        alert(error)
+      })
+  }, [])
+
+  return (
+    <div className="App">
+      <div className="blog_title">Open City Blog</div>
+      <div className="app_posts">
+        {posts.map((post) => (
+          <Post post={post} key={post.id} />
+        ))}
+      </div>
+      <div className="new_post">
+        <NewPost />
+      </div>
+    </div>
+  )
+}
+
+export default App
