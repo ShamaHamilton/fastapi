9bc6d57211c7f21cfd0e4c03f05c390e3643ae2b
Шамиль Рамазанов <shamil.ramazanov.1994@mail.ru>
2025-01-26 00:37:19 +0300

    Blog site: Added logger.

--- fastapi-blog/backend/main.py
+++ fastapi-blog/backend/main.py
@@ -1,12 +1,21 @@
+import asyncio
+
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+# from starlette.middleware.base import BaseHTTPMiddleware
+
 
 from database import models
 from database.database import engine
 from routers import post
 
+from logger import logger
+# from middleware import log_middleware
+
 app = FastAPI()
+# app.add_middleware(BaseHTTPMiddleware, dispatch=log_middleware)
+logger.info('Starting API...')
 
 app.include_router(post.router)
 
@@ -26,3 +35,16 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*']
 )
+
+
+# @app.get('/')
+# async def index() -> dict:
+#     # logger.info('Request to index page')
+#     await asyncio.sleep(1.5)
+#     return {'message': 'Hello'}
+
+
+# @app.get('/upload-videos')
+# async def upload_videos() -> dict:
+#     # logger.info('Request to video-upload page')
+#     return {'message': 'Video Uploaded'}
